79d841c5422eea4408e6e80690be003ad3a157d3
Ali Mirghasemi <ali.mirghasemi1376@gmail.com>
2021-08-28 01:15:11 +0430

    example tested

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -25,12 +25,19 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "InputStream.h"
+#include "OutputStream.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
-
+typedef struct {
+	UART_HandleTypeDef* 		HUART;
+	DMA_HandleTypeDef*			RxDMA;
+	DMA_HandleTypeDef*			TxDMA;
+	IStream									Input;
+	OStream									Output;
+} UARTStream;
 /* USER CODE END PTD */
 
 /* Private define ------------------------------------------------------------*/
@@ -45,13 +52,25 @@
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
+static const char CRLF[2] = "\r\n";
 
+extern DMA_HandleTypeDef hdma_usart1_rx;
+extern DMA_HandleTypeDef hdma_usart1_tx;
+
+static uint8_t	streamRxBuff[10];
+static uint8_t	streamTxBuff[10];
+
+static UARTStream stream;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 /* USER CODE BEGIN PFP */
-
+void UARTStream_stopReceiveDMA(UARTStream* stream);
+void UARTStream_stopTransmitDMA(UARTStream* stream);
+Stream_LenType UARTStream_checkReceivedBytes(IStream* stream);
+void UARTStream_receive(IStream* stream, uint8_t* buff, Stream_LenType len);
+void UARTStream_transmit(OStream* stream, uint8_t* buff, Stream_LenType len);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -66,7 +85,7 @@ void SystemClock_Config(void);
 int main(void)
 {
   /* USER CODE BEGIN 1 */
-
+	char str[32];
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
@@ -90,13 +109,40 @@ int main(void)
   MX_DMA_Init();
   MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
-
+	
+	// init uart stream
+	stream.HUART = &huart1;
+	stream.RxDMA = &hdma_usart1_rx;
+	stream.TxDMA = &hdma_usart1_tx;
+  IStream_init(&stream.Input, UARTStream_receive, streamRxBuff, sizeof(streamRxBuff));
+	IStream_setCheckReceive(&stream.Input, UARTStream_checkReceivedBytes);
+	IStream_setArgs(&stream.Input, &stream);
+	OStream_init(&stream.Output, UARTStream_transmit, streamTxBuff, sizeof(streamTxBuff));
+	OStream_setArgs(&stream.Output, &stream);
+	// start receive
+	IStream_receive(&stream.Input);
+	
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+		if (IStream_available(&stream.Input) > 0) {
+			// Way 1
+			Stream_LenType len = IStream_readBytesUntilPattern(&stream.Input, (uint8_t*) CRLF, sizeof(CRLF), (uint8_t*) str, sizeof(str));
+			if (len > 0) {
+				OStream_writeBytes(&stream.Output, (uint8_t*) str, len);
+				OStream_flush(&stream.Output);
+			}
+			
+			// Way 2
+			/*Stream_LenType lineLen = IStream_findPattern(&stream.Input, (uint8_t*) CRLF, sizeof(CRLF));
+			if (lineLen > 0) {
+				OStream_writeStream(&stream.Output, &stream.Input, lineLen + sizeof(CRLF));
+				OStream_flush(&stream.Output);
+			}*/
+		}
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -149,6 +195,74 @@ void SystemClock_Config(void)
 }
 
 /* USER CODE BEGIN 4 */
+void UARTStream_stopReceiveDMA(UARTStream* stream) {
+	UART_HandleTypeDef* huart = stream->HUART;
+	uint32_t dmarequest = HAL_IS_BIT_SET(huart->Instance->CR3, USART_CR3_DMAR);
+  if ((huart->RxState == HAL_UART_STATE_BUSY_RX) && dmarequest)
+  {
+    CLEAR_BIT(huart->Instance->CR3, USART_CR3_DMAR);
+
+    /* Abort the UART DMA Rx stream */
+    if (huart->hdmarx != NULL)
+    {
+      HAL_DMA_Abort(huart->hdmarx);
+    }
+    /* Disable RXNE, PE and ERR (Frame error, noise error, overrun error) interrupts */
+		CLEAR_BIT(huart->Instance->CR1, (USART_CR1_RXNEIE | USART_CR1_PEIE));
+		CLEAR_BIT(huart->Instance->CR3, USART_CR3_EIE);
+
+		/* At end of Rx process, restore huart->RxState to Ready */
+		huart->RxState = HAL_UART_STATE_READY;
+  }
+}
+void UARTStream_stopTransmitDMA(UARTStream* stream) {
+	UART_HandleTypeDef* huart = stream->HUART;
+	uint32_t dmarequest = HAL_IS_BIT_SET(huart->Instance->CR3, USART_CR3_DMAT);
+  if ((huart->gState == HAL_UART_STATE_BUSY_TX) && dmarequest)
+  {
+    CLEAR_BIT(huart->Instance->CR3, USART_CR3_DMAT);
+
+    /* Abort the UART DMA Tx stream */
+    if (huart->hdmatx != NULL)
+    {
+      HAL_DMA_Abort(huart->hdmatx);
+    }
+    /* Disable TXEIE and TCIE interrupts */
+		CLEAR_BIT(huart->Instance->CR1, (USART_CR1_TXEIE | USART_CR1_TCIE));
+
+		/* At end of Tx process, restore huart->gState to Ready */
+		huart->gState = HAL_UART_STATE_READY;
+  }
+}
+Stream_LenType UARTStream_checkReceivedBytes(IStream* stream) {
+	UARTStream* uartStream = (UARTStream*) IStream_getArgs(stream);
+	return IStream_incomingBytes(stream) - uartStream->RxDMA->Instance->NDTR;
+}
+void UARTStream_receive(IStream* stream, uint8_t* buff, Stream_LenType len) {
+	UARTStream* uartStream = (UARTStream*) IStream_getArgs(stream);
+	UARTStream_stopReceiveDMA(uartStream);
+	HAL_UART_Receive_DMA(uartStream->HUART, buff, len);
+}
+void UARTStream_transmit(OStream* stream, uint8_t* buff, Stream_LenType len) {
+	UARTStream* uartStream = (UARTStream*) OStream_getArgs(stream);
+	UARTStream_stopTransmitDMA(uartStream);
+	HAL_UART_Transmit_DMA(uartStream->HUART, buff, len);
+}
+
+void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart) {
+	switch ((uint32_t) huart->Instance) {
+		case USART1_BASE:
+			OStream_handle(&stream.Output, OStream_outgoingBytes(&stream.Output));
+			break;
+	}
+}
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
+	switch ((uint32_t) huart->Instance) {
+		case USART1_BASE:
+			IStream_handle(&stream.Input, IStream_incomingBytes(&stream.Input));
+			break;
+	}
+}
 
 /* USER CODE END 4 */
 
